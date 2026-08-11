fb5710b102e2e3baf8cb61f87412ae7825144d49
Łukasz Czajka <lukaszcz@mimuw.edu.pl>
2026-08-11 10:46:12 +0200

    test: enforce release version lockstep

--- tests/test_version.py
+++ tests/test_version.py
@@ -3,9 +3,22 @@
 from __future__ import annotations
 
 import re
+from importlib.metadata import version
 
+from agm.packages.manifest import load_manifest
+from agm.stdlib_locator import shipped_stdlib_root
 from agm.version import AGM_VERSION
 
 
 def test_agm_version_has_plain_semver_shape() -> None:
     assert re.fullmatch(r"[0-9]+\.[0-9]+\.[0-9]+", AGM_VERSION)
+
+
+def test_release_metadata_matches_runtime_version() -> None:
+    assert version("agm") == AGM_VERSION
+
+
+def test_shipped_standard_library_matches_runtime_version() -> None:
+    manifest = load_manifest(shipped_stdlib_root() / "package.toml")
+
+    assert str(manifest.version) == AGM_VERSION

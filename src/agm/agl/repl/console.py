a073bfd6dce9d70158452ef1b344206945d9e8ce
Łukasz Czajka <lukaszcz@mimuw.edu.pl>
2026-09-15 17:04:11 +0200

    fix: restore full branch coverage of repl token span filtering

--- src/agm/agl/repl/console.py
+++ src/agm/agl/repl/console.py
@@ -314,10 +314,7 @@ def _styled_spans(
     for index, token in enumerate(tokens):
         start = token.start_pos
         end = token.end_pos
-        if start is None or end is None:
-            continue
-        end = min(end, next_starts[index])
-        if end <= start:
+        if start is None or end is None or (end := min(end, next_starts[index])) <= start:
             continue
         style = forced.get(index)
         if style is None:
